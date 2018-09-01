55ba3eb649d3085f1abc46defe02901c5ade2c41
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-09-01 18:18:05 -0400

    Function roll(..) returns the outcome of simulating a die roll

--- pig.go
+++ pig.go
@@ -15,7 +15,16 @@ type score struct {
 // An action transitions stochastically to a resulting score
 type action func(current score) (result score, turnIsOver bool)
 
-
+// roll returns the (result, turnIsOver) outcome of simulating a die roll.
+// If the roll value is 1, then this Turn score is abandone, and the player's 
+// roles swap. Otherwise, the roll value is added to thisTurn.
+func roll(s score) (score, bool) {
+	outcome := rand.Intn(6) + 1 // A random int in [1, 6]
+	if outcome == 1 {
+		return score{s.opponent, s.player, 0}, true
+	}
+	return score{s.player, s.component, outcome + s.thisTurn, 0}, true
+}
 
 function main() {
 
